daee5565431016482162b684330367450c8a018f
hawk <18801353760@163.com>
2024-02-20 14:00:57 +0800

    include/yaf.h: use do-while instead of block statment for log macro

--- include/yaf.h
+++ include/yaf.h
@@ -6,16 +6,16 @@
     #ifdef __KERNEL__
         #include <linux/printk.h>
         #include <linux/kern_levels.h>
-        #define log(args...) { \
+        #define log(args...) do { \
             printk(KERN_DEFAULT args); \
             printk(KERN_CONT "\n"); \
-        }
+        } while(0)
     #else // __KERNEL__
         #include <stdio.h>
-        #define log(args...) { \
+        #define log(args...) do { \
             printf(args); \
             putchar('\n'); \
-        }
+        } while(0)
     #endif // __KERNEL__
 
 #endif // __YAF_H_
